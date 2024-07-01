17abac7444c99054f17e3f305784ab6a1ab4856c
Vineet Sinha <vineet.sinha@gmail.com>
2024-07-01 00:13:34 -0400

    integrating with openai

--- app/api/chatSessions/getResponse/route.ts
+++ app/api/chatSessions/getResponse/route.ts
@@ -1,16 +1,97 @@
 import { NextRequest } from 'next/server'
+import { OpenAI } from 'openai'
 import prisma from '@/lib/db'
+import Typed, { Prisma, MessageRole } from '@prisma/client';
+
+let openAIApi: OpenAI
+if (process.env.OPENAI_API_KEY) {
+  openAIApi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+}
+
+const systemPrompt = ''
+
+async function callOpenAI(messages: Typed.Message[], message: string) {
+  const msgsForLLM: OpenAI.ChatCompletionMessageParam[] = [
+    { role: 'system', content: systemPrompt },
+    ...messages,
+    { role: 'user', content: message },
+  ]
+  const response = await openAIApi.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: msgsForLLM,
+  })
+
+  if (
+    response &&
+    response.choices &&
+    response.choices.length > 0 &&
+    response.choices[0].message &&
+    response.choices[0].message.content
+  ) {
+    return response.choices[0].message.content
+  } else {
+    return `Sorry, didn't hear back from LLM!`
+  }
+  // return 'Lets do it!';
+}
 
 export async function POST(req: NextRequest) {
   const { userId, chatSessionId, message } = await req.json();
 
   try {
-    // provide a dummy response for now
-    // no need for saving messages
-    return Response.json({
-      success: true,
-      message: 'Lets do it!'
-    });
+    // save message and return
+    if (chatSessionId) {
+      const chatSession = await prisma.chatSession.findUnique({
+        where: { id: chatSessionId }
+      })
+      if (!chatSession) {
+        return Response.json(
+          { message: `Failed to find chat sessions: ${chatSessionId}` },
+          { status: 500 }
+        )
+      }
+      const response = await callOpenAI(chatSession.messages, message);
+      chatSession.messages.push({
+        role: MessageRole.user,
+        content: message,
+        createdAt: new Date()
+      }, {
+        role: MessageRole.assistant,
+        content: response,
+        createdAt: new Date(),
+      })
+      await prisma.chatSession.update({
+        where: { id: chatSessionId },
+        data: chatSession,
+      });
+
+      return Response.json({
+        success: true,
+        message: response
+      });
+    } else {
+      // create new chat session
+      const response = await callOpenAI([], message);
+      const createInp:Prisma.ChatSessionUncheckedCreateInput = {
+        userId,
+        messages: [{
+          role: MessageRole.user,
+          content: message,
+        }, {
+          role: MessageRole.assistant,
+          content: response,
+        }]
+      };
+      await prisma.chatSession.create({
+        data: createInp
+      });
+
+      return Response.json({
+        success: true,
+        message: response
+      });
+    }
+
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error('Error fetching chat sessions:', error.message)
