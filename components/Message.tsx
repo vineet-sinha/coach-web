479ab628a78c3e6eb8332606c597ef75d36deaa0
Vineet Sinha <vineet.sinha@gmail.com>
2024-07-01 00:40:38 -0400

    minor formatting for bot responses

--- components/Message.tsx
+++ components/Message.tsx
@@ -6,12 +6,12 @@ const Message: React.FC<Typed.Message> = ({ content, role, createdAt }) => {
   const isUser = role === 'user';
 
   return (
-    <div className={`message ${isUser ? 'user-message' : 'bot-message'}`}>
+    <div className={`message ${isUser ? 'user-message' : 'bot-message text-green-600'}`}>
       <div className="message-content">
         <p>{content}</p>
       </div>
       <div className="message-timestamp">
-        <p>{intlFormatDistance(createdAt, new Date())}</p>
+        <p className='italic text-sm'>- {intlFormatDistance(createdAt, new Date())}</p>
       </div>
     </div>
   );
