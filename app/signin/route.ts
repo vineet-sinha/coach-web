119045e5aa3fbbcc796627520e8a799b41381f6f
Vineet Sinha <vineet.sinha@gmail.com>
2024-07-08 06:28:31 -0400

    get the user to chat after they login

--- app/signin/route.ts
+++ app/signin/route.ts
@@ -2,5 +2,5 @@ import { NextRequest } from 'next/server'
 import { signIn } from '@/auth'
 
 export async function GET(req: NextRequest) {
-  await signIn('google', { redirectTo: '/', redirect: true })
+  await signIn('google', { redirectTo: '/chat', redirect: true })
 }
